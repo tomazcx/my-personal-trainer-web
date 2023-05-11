459b613351bfbc0c20e7873dc185ee714448c9e0
tomazcx <tomazcx06@gmail.com>
2023-05-11 10:27:03 -0300

    feat: added recover password api integration

--- src/pages/forgot-password.tsx
+++ src/pages/forgot-password.tsx
@@ -1,33 +1,15 @@
 import {EmailPasswordRecoverForm} from '@components/EmailPasswordRecoverForm'
-import {RecoverPasswordForm} from '@components/RecoverPasswordForm'
 import {TokenPasswordRecoverForm} from '@components/TokenPasswordRecoverForm'
 import {useState} from 'react'
 
 const ForgotPassword = () => {
 
-	const [formToRender, setSelectedForm] = useState(0)
-
-	let form: React.ReactNode = null
-
-	switch (formToRender) {
-		case 0:
-			form = <EmailPasswordRecoverForm setForm={setSelectedForm} />
-			break;
-		case 1:
-			form = <TokenPasswordRecoverForm setForm={setSelectedForm} />
-			break;
-		case 2:
-			form = <RecoverPasswordForm setForm={setSelectedForm} />
-			break;
-
-
-
-	}
+	const [formToRender, setSelectedForm] = useState(1)
 
 	return (
 		<main className="">
 			<div className='h-screen w-screen bg-forgot-password bg-cover fixed inset-0 grayscale'></div>
-			{form}
+			{formToRender === 0 ? <EmailPasswordRecoverForm setForm={setSelectedForm} /> : <TokenPasswordRecoverForm setForm={setSelectedForm} />}
 		</main>
 	)
 
